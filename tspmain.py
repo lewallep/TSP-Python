55fcb5fa9fe712c2499f2c623d64ffd3be00bf71
lewallep <fefifophil@hotmail.com>
2018-05-31 15:15:00 -0700

    Finished gen_test.py v1.  Started tspmain.py

---
+++ tspmain.py
@@ -0,0 +1,31 @@
+import os, sys
+
+# import the cities from the text file
+# Returns a list of cities.  Cities = named tuple?
+def importCase(filePath):
+	cities = []
+	return cities
+
+
+
+# Keeps track of the finished tours.
+# Returns a list of the start id and the length.
+# Later this could be deprecated with a database output of each tour.
+
+
+
+# Single threaded tour.
+# Returns the total distance of the tour.
+# Returns the order of the cities as a list
+
+
+# Processes the results in real time.
+# Each iteration update the lowest overall tour length.
+
+# wrapper loop that takes iterates over the import cases and passes in the start ID
+def singleThreadedTsp(cities):
+
+if __name__ == '__main__':
+	print("tspmain.py  has run.")
+	cities = importCase(filePath)
+	singleThreadedTsp(cities)
