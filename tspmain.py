3f7ab7a8b77de1d6666d7466c57124e9f73f6499
lewallep <fefifophil@hotmail.com>
2018-06-04 15:39:55 -0700

    Single threaded tour is working.

--- tspmain.py
+++ tspmain.py
@@ -1,4 +1,4 @@
-import os, sys, copy
+import os, sys, copy, math
 from collections import namedtuple
 
 # import the cities from the text file
@@ -23,28 +23,62 @@ def importCase():
 # Returns the total distance of the tour.
 # Returns the order of the cities as a list
 def tour(cities, startCity):
-	print("inside tour. . . . . . . . . . .  ..")
-	print(cities)
+	#print("inside tour. . . . . . . . . . .  ..")
+	#print(cities)
+	
 	localCities = copy.deepcopy(cities)
 	tour = []
-	distance = sys.maxsize
-	print("len(City): %s" % (len(localCities)))
-	print("startCity: " + str(startCity))
+	tourDistance = 0
+	#print("len(City): %s" % (len(localCities)))
+	#print("startCity: " + str(startCity))
 
-	curCity = localCities[startCity]
-	localCities.pop(startCity) 
+	curCity = localCities.pop(startCity) 
 	# print(curCity)
 	# print(cities)
 	tour.append(curCity)
 
 	while len(localCities) > 0:
-		# Find next closest city
-		print("localCities length: %s" % len(localCities))
-		tour.append(localCities[0])
-		localCities.pop(0)
+		# Find next closest city in the localCities list./
+		nextCity, distToNext, index = nextCityDist(localCities, curCity)
+		#print("nextCity: " + str(nextCity))
+
+		# print("localCities length: %s" % len(localCities))
+		# tour.append(localCities[0])
+		
+		tour.append(nextCity)
+		localCities.pop(index)
+		tourDistance += distToNext
 
 	# print(tour)
-	return distance, tour
+	return tourDistance, tour
+
+
+# Find distance to the next closest city
+# Returns the distance to the next city and the ID of the next cloest city.
+def nextCityDist(localCities, curCity):
+	distToNext = sys.maxsize
+	curClosest = sys.maxsize 
+	#print("nextCityDist() curCity: " + str(curCity))
+
+	# Iterate over the existing cities
+	for i in range(len(localCities)):
+		curId = curCity.id
+		curX = curCity.x
+		curY = curCity.y
+
+		# n is a prefix for next potential city.
+		nx = localCities[i].x
+		ny = localCities[i].y
+		nid = localCities[i].id
+
+		curClosest = math.sqrt((nx*nx) + (ny*ny))
+		if distToNext > curClosest:
+			distToNext = curClosest
+			nextCity = localCities[i]
+			index = i
+
+	return nextCity, distToNext, index
+
 
 
 # Processes the results in real time.
@@ -59,16 +93,23 @@ def singleThreadedTsp(cities):
 	curDistance = sys.maxsize
 	curTour = []
 	shortestTour = []
-	results = []
+	results = [] # May not be needed
 
 	#print(shortestTour)
 	for i in range(len(cities)):
 		curDistance, curTour = tour(cities, i)
+		#print("curTour: " + str(curTour))
+		print("curDistance: " + str(curDistance))
 		curTour.insert(0, curDistance)
 		results.append(curTour)
 		if curDistance < shortestDist:
 			shortestDist = curDistance
 			shortestTour = curTour
+
+	print("shortestDist: " + str(shortestDist))
+	print("shortestTour: " + str(shortestTour))
+	print("results: ")
+	print(results)
 
 if __name__ == '__main__':
 	print("tspmain.py  has run.")
