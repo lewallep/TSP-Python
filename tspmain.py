ae322b2bb4abb9a4c65851fa2130716f63a74345
lewallep <fefifophil@hotmail.com>
2018-06-05 15:51:13 -0700

    Progress commit

--- tspmain.py
+++ tspmain.py
@@ -80,10 +80,6 @@ def nextCityDist(localCities, curCity):
 	return nextCity, distToNext, index
 
 
-
-# Processes the results in real time.
-# Each iteration update the lowest overall tour length.
-
 # wrapper loop that takes iterates over the import cases and passes in the start ID
 # Returns a list with all of the tours and shortest tour.
 def singleThreadedTsp(cities):
@@ -106,14 +102,67 @@ def singleThreadedTsp(cities):
 			shortestDist = curDistance
 			shortestTour = curTour
 
-	print("shortestDist: " + str(shortestDist))
+	#sprint("shortestDist: " + str(shortestDist))
 	print("shortestTour: " + str(shortestTour))
-	print("results: ")
-	print(results)
+	#print("results: ")
+	#print(results)
+	return results
+
+
+# Ensure every city is only visited once in ONE tour the finished tour.
+# Check for any duplicate ID's.
+# Returns True if the results have no duplicate ID's.
+def checkResultsAll(results):
+	goodResults = False
+
+	# Skip the tour distance at the beginning of each result
+	for i in range(len(results)):
+		tour = results[i]
+		for city in range(1, len(tour)):
+			print("city in tour: " + str(tour[city]))
+		print(results[i][0])
+		print()
+
+	return goodResults
+
+# Takes a tour and it's distance and checks to see if any of the edges 
+# intersect each other.
+# Only checks a subset of 4 connected vertexes at a time.
+# Ideally this would be connected to the tours to identify and unwind intersections
+# as the nearest neighbors are found.
+
+
+
+# Finds and checks only the shortest tour from the list of results.
+# Not especially fast as the results do not come back sorted in ascending order.
+def checkResultShortest(results):
+	bestDist = sys.maxsize
+	bestIndex = None
+	bestTour = []
+	idDict = {}
+
+	for i in range(len(results)):
+		if results[i][0] < bestDist:
+			bestDist = results[i][0]
+			bestIndex = i
+
+	bestTour = results[bestIndex]
+	for z in range(1, len(bestTour)):
+		if str(bestTour[z].id) in idDict:
+			print("Duplicate City id found.  Results are invalid.")
+			return False
+		else:
+			idDict[str(bestTour[z].id)] = 1
+
+	# If this return statment is true no duplicate ID's found.
+	return True
+
 
 if __name__ == '__main__':
 	print("tspmain.py  has run.")
 	cities = importCase()
 	# print("main cities")
 	# print(cities)
-	singleThreadedTsp(cities)
+	results = singleThreadedTsp(cities)
+	if checkResultShortest(results):
+		print("All good in the hood.")
